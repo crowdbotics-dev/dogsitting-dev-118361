1eb99bc97f9ce93c48e2e11bf832270aff154f42
Crowdbotics <engineering@crowdbotics.com>
2023-09-25 21:07:05 +0000

    Initial commit

---
+++ backend/dogsitting_dev_118361/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for dogsitting_dev_118361 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "dogsitting_dev_118361.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dogsitting-dev-118361",
+  url: "https://dogsitting-dev-118361.botics.co",
+  api: "https://dogsitting-dev-118361.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
