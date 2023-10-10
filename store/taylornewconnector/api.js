1580af9c023aab91a8640a1657a8092ccbc3ccdb
Crowdbotics <engineering@crowdbotics.com>
2023-10-10 21:45:47 +0000

    Update store.

--- store/taylornewconnector/api.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-const taylornewconnector = axios.create({
-  baseURL: "https://www3.google.ca",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
-export const apiService = {}
