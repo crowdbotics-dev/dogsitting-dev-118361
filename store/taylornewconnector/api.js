5d419a116ed02ed4c67bedf8be98142381dcb488
Crowdbotics <engineering@crowdbotics.com>
2023-10-12 18:13:00 +0000

    Update store.

--- store/taylornewconnector/api.js
+++ store/taylornewconnector/api.js
@@ -3,9 +3,15 @@ const taylornewconnector = axios.create({
   baseURL: "https://www.google.ca",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
+function taylornewconnector4_get__read(payload) {
+  return taylornewconnector.get(`/`)
+}
 function taylornewconnector4_get_blah_123_read(payload) {
   return taylornewconnector.get(`/blah/123`, {
-    params: { new_param: payload.new_param }
+    params: { new_param: payload.new_param, new_param_2: payload.new_param_2 }
   })
 }
-export const apiService = { taylornewconnector4_get_blah_123_read }
+export const apiService = {
+  taylornewconnector4_get__read,
+  taylornewconnector4_get_blah_123_read
+}
---
+++ store/taylornewconnector/taylornewconnector4_response_get_DataCallNames.slice.js
@@ -0,0 +1,55 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
+import { apiService } from "./api"
+export const taylornewconnector4_get_blah_123_read = createAsyncThunk(
+  "taylornewconnector4_response_get_DataCallNames/taylornewconnector4_get_blah_123_read",
+  async payload => {
+    const response = await apiService.taylornewconnector4_get_blah_123_read(
+      payload
+    )
+    return response.data
+  }
+)
+const initialState = { entities: [], api: { loading: "idle", error: null } }
+const taylornewconnector4_response_get_DataCallNamesSlice = createSlice({
+  name: "taylornewconnector4_response_get_DataCallNames",
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(
+        taylornewconnector4_get_blah_123_read.pending,
+        (state, action) => {
+          if (state.api.loading === "idle") {
+            state.api.loading = "pending"
+          }
+        }
+      )
+      .addCase(
+        taylornewconnector4_get_blah_123_read.fulfilled,
+        (state, action) => {
+          if (state.api.loading === "pending") {
+            state.entities = [
+              ...state.entities.filter(
+                record => record.id !== action.payload.id
+              ),
+              action.payload
+            ]
+            state.api.loading = "idle"
+          }
+        }
+      )
+      .addCase(
+        taylornewconnector4_get_blah_123_read.rejected,
+        (state, action) => {
+          if (state.api.loading === "pending") {
+            state.api.error = action.error
+            state.api.loading = "idle"
+          }
+        }
+      )
+  }
+})
+export default {
+  taylornewconnector4_get_blah_123_read,
+  slice: taylornewconnector4_response_get_DataCallNamesSlice
+}
