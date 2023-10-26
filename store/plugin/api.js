28173a42ac2fe7b6479b1d73edde8cec87a311a8
Crowdbotics <engineering@crowdbotics.com>
2023-10-26 21:21:02 +0000

    Update store.

---
+++ store/plugin/api.js
@@ -0,0 +1,12 @@
+import axios from "axios"
+import { PLUGIN_70_TOKEN } from "react-native-dotenv"
+const plugin = axios.create({
+  baseURL:
+    "https://crowdbotics-slack-dev.herokuapp.com/dashboard/app/7155/storyboard/8204/",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${PLUGIN_70_TOKEN}`
+  }
+})
+export const apiService = {}
