0422c80311cdc361639a3b94baad13cdbee39935
Crowdbotics <engineering@crowdbotics.com>
2023-10-26 21:12:04 +0000

    Update store.

---
+++ store/taylornewtokeninheader/api.js
@@ -0,0 +1,11 @@
+import axios from "axios"
+import { TAYLOR_NEW_TOKEN_IN_HEADER_TOKEN } from "react-native-dotenv"
+const taylornewtokeninheader = axios.create({
+  baseURL: "https://www.google.ca",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${TAYLOR_NEW_TOKEN_IN_HEADER_TOKEN}`
+  }
+})
+export const apiService = {}
